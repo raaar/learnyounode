2ac4ef19a3a5f5b593be4725986cbc62b9802b8e
raaar <raffaelefacondini@raf.local>
2013-12-05 00:35:09 +0000

    9

---
+++ 8/method2.js
@@ -0,0 +1,19 @@
+/* 
+	SOLUTION NUMBER TWO
+*/
+
+var http = require('http');
+var bl = require('bl');
+var url = process.argv[2];
+
+
+http.get( url , function(res){
+	res.pipe(bl(function (err, data) { 
+		if(err)
+			return console.error(data);
+		data = data.toString();
+  		console.log(data.length);
+    	console.log(data.toString())
+	}));
+})
+
--- 9/program.js
+++ 9/program.js
@@ -4,6 +4,19 @@ var url_one = process.argv[2];
 var url_two = process.argv[3]; 
 var url_three = process.argv[4]; 
 
+var bl = require('bl');
+
 console.log(url_one);
 console.log(url_two);
 console.log(url_three);
+
+
+/*
+http.get( url_one , function (res) {
+	res.setEncoding("utf8");
+	res.on("data", function(item){
+		console.log(item);
+	})
+})
+*/
+http.get(url_one)
